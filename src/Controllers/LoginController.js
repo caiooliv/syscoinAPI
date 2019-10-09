52345fc2a1801e2e4779caa3585872ac394f2c5e
Caio Oliveira de Moraes <caiooliv97@gmail.com>
2019-10-09 20:46:21 -0300

    Criada Api

---
+++ src/Controllers/LoginController.js
@@ -0,0 +1,10 @@
+module.exports = {
+    store(req,res){
+        if(req.query.user == 'syscoin' && req.query.password == 'meEscolhe' ){
+            return  res.json({"mensagem": "Meu nome completo é Caio Oliveira. Eu quero essa vaga!"});
+          } else {
+              return res.json({"mensagem": "Credenciais erradas."});
+          }
+    }
+
+};
